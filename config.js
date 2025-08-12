325de5165849af6cce88f52eb296690560dc06dc
ahmedlafialhrees <a7mdq822@hotmail.com>
2025-08-12 15:05:56 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// غيّر هذا لعنوان سيرفرك الحقيقي (Render/Railway)
+window.SERVER_URL = "https://YOUR-RENDER-APP.onrender.com";
+
+// الأونر الرئيسي اللي تظهر له لوحة التحكم
+window.MAIN_OWNER_NAME = "احمد";
