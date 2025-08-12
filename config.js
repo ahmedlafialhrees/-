3e83d7c5645dc4fae5e2254189794b67083f6413
ahmedlafialhrees <a7mdq822@hotmail.com>
2025-08-12 17:20:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-window.SERVER_URL = "https://YOUR-RENDER-APP.onrender.com";
-window.MAIN_OWNER_NAME = ""; // ما عاد نستخدمه
+// config.js
+export const SERVER_URL = "https://YOUR_SERVER_URL_HERE"; // عدّلها إذا لزم
+export const OWNER_NAME = "احمد";   // اسم الأونر الرئيسي بالضبط
+export const OWNER_PASS = "6677";   // رقم سر الأونر الرئيسي
